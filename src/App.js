39ce21b238f58f72a14d6b9f564721992d515ddc
Michael Mallowe <mmallowe@gmail.com>
2020-04-20 17:55:40 -0700

    passing alert with onCopy

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import './App.css';
 // import Lottery from './Lottery';
 // import CoinContainer from './CoinContainer'
 // import BoxContainer from './BoxContainer'
-import WiseSquare from './WiseSquare'
+// import WiseSquare from './WiseSquare'
+import CopyDemo from './CopyDemo'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <WiseSquare/>
+        <CopyDemo/>
 
         {/* <Lottery title='Mini Daily' maxNum={10} maxBalls={3} /> */}
       </header>
