0a9e1e24905925dc371eaecd0ad36a3e5775aa55
Michael Mallowe <mmallowe@gmail.com>
2020-07-03 23:34:12 -0700

    editing form for item, setState for form

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,6 +7,7 @@ class TodoList extends Component {
         this.state = {todos: []}
         this.create = this.create.bind(this)
         this.remove = this.remove.bind(this)
+        this.update = this.update.bind(this)
     }
     // add method to add the list item into state and list
     create(newTodo) {
@@ -21,9 +22,26 @@ class TodoList extends Component {
             todos: this.state.todos.filter(t => t.id !== id)
         })
     }
+    update(id, updatedTask) {
+        const updatedTodos = this.state.todos.map(todo => {
+            if(todo.id === id) {
+                return {...todo, task: updatedTask}
+            }
+            return todo
+        })
+        this.setState({ todos: updatedTodos })
+    }
     render() {
         const todos = this.state.todos.map(todo => {
-            return <Todo key={todo.id} id={todo.id} task={todo.task} removeTodo={this.remove} />
+            return (
+                <Todo 
+                    key={todo.id} 
+                    id={todo.id} 
+                    task={todo.task} 
+                    removeTodo={this.remove}
+                    updatedTodo={this.update}
+                />
+            ) 
         })
         return (
             <div className='Ball'>
